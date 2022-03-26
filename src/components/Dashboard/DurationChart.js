ac41ed609d1fc43dcc5eb0bacf6d3994986840c6
meithnav <meithnavlakha@gmail.com>
2022-03-26 21:14:54 +0530

    dashboard

--- src/components/Dashboard/DurationChart.js
+++ src/components/Dashboard/DurationChart.js
@@ -1,9 +1,64 @@
 import React from 'react'
+import { Chart as ChartJS, registerables } from "chart.js";
+import { Line, Bar, Chart } from "react-chartjs-2";
+ChartJS.register(...registerables);
+
+
 
 export default function DurationChart() {
+
+   
   return (
     <div className="durationChart">
-      <h1>DurationChart</h1>
+      <h1>Sleep Stage Chart</h1>
+      <Line
+        data={{
+          labels: ["MON", "TUE", "WED", "THUR", "FRI", "SAT", "SUN"],
+          datasets: [
+            {
+              label: "DAILY SLEEP HOURS PER WEEK",
+              data: [6, 10, 3, 5, 6, 3, 7],
+              backgroundColor: [
+                "rgba(25, 99, 132, 0.8)",
+                "rgba(54, 162, 235, 0.8)",
+                "rgba(255, 206, 86, 0.8)",
+                "rgba(75, 192, 192, 0.8)",
+                "rgba(153, 102, 255, 0.8)",
+                "rgba(255, 159, 64, 0.8)",
+                "rgba(225, 99, 132, 0.8)",
+              ],
+              borderColor: [
+                "rgba(25, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)",
+                "rgba(225, 99, 132, 0.8)",
+              ],
+              borderWidth: 1,
+            },
+            
+
+          ],
+        }}
+        options={{
+          maintainAspectRatio: true,
+          scales: {
+            y: {
+                // max: 24,
+                min:0,
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+          
+          },
+        }}
+        height={180}
+        width={380}
+      />
     </div>
   );
 }
+
--- src/components/Dashboard/PieChart.js
+++ src/components/Dashboard/PieChart.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Line } from "react-chartjs-2";
+
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+import {pie} from '../../data/data' 
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function PieChart() {
+
+
+
   return (
     <div className="pieChart">
       <h1>Pie Chart</h1>
+      <Pie data={pie} redraw={true}  />
     </div>
   );
+
+
 }
+
+
+ 
